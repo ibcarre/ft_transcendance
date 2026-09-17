5928f46fcfcf296e7f5dcf04a34e642891e5a597
Benjamin <benjamsc@student.42.fr>
2026-09-17 03:34:52 +0200

    (fix-make) remove unused services in compose.dev

--- srcs/backend/index.js
+++ srcs/backend/index.js
@@ -5,7 +5,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', 
-   (req, res) => res.send('Dockerizing express Application'))
+   (req, res) => res.send('Dockerizing Node Application'))
 
 app.listen(5000, 
    () => console.log(`⚡️[bootup]: Server is running at port: 5000`));
