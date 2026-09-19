da70f3ed9d2be2cae2141ecda3614d90da03ce1b
Benjamin <benjamsc@student.42.fr>
2026-09-19 14:41:10 +0200

    fix(postgres) add default tables values

--- srcs/infra/postgres/init_sql/01_schema.sql
+++ srcs/infra/postgres/init_sql/01_schema.sql
@@ -8,12 +8,12 @@ CREATE TABLE users (
 );
 
 CREATE TABLE games (
-	game_id		uuid PRIMARY KEY,
-	status		text NOT NULL,
+	game_id		uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	status		text NOT NULL DEFAULT 'playing',
 	CONSTRAINT	chk_status CHECK (status IN ('playing', 'finished', 'cancelled')),
-	goal		int NOT NULL,
+	goal		int NOT NULL DEFAULT 100,
 	CONSTRAINT	chk_goalLimit CHECK (goal >= 50 AND goal <= 1000),
-	date		timestamptz NOT NULL
+	created_at	timestamptz NOT NULL DEFAULT now()
 );
 
 
