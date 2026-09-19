402a9044adf73737913ef429c16a0f4bc10206f7
Benjamin <benjamsc@student.42.fr>
2026-09-19 15:30:30 +0200

    fix(postgres) add users_in_game and friends tables, next one MPs

--- srcs/infra/postgres/init_sql/01_schema.sql
+++ srcs/infra/postgres/init_sql/01_schema.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE users (
-	user_id		uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	id			uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	email		text NOT NULL UNIQUE,
 	name		text NOT NULL,
 	password	text NOT NULL,
@@ -8,12 +8,36 @@ CREATE TABLE users (
 );
 
 CREATE TABLE games (
-	game_id		uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	id			uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	status		text NOT NULL DEFAULT 'playing',
 	CONSTRAINT	chk_status CHECK (status IN ('playing', 'finished', 'cancelled')),
 	goal		int NOT NULL DEFAULT 100,
 	CONSTRAINT	chk_goalLimit CHECK (goal >= 50 AND goal <= 1000),
 	created_at	timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE users_in_game (
+	id			bigserial PRIMARY KEY,
+	user_id		uuid NOT NULL REFERENCES users(id),
+	game_id		uuid NOT NULL REFERENCES games(id),
+	CONSTRAINT	uq_game_user UNIQUE (game_id, user_id),
+	seat		int NOT NULL DEFAULT 0,
+	CONSTRAINT	uq_game_seats UNIQUE (game_id, seat),
+	CONSTRAINT	chk_maxSeat CHECK (seat >= 0 AND seat <= 3),
+	score		int NOT NULL DEFAULT 0
+);
+
+CREATE TABLE friends (
+	id			  bigserial PRIMARY KEY,
+	user_id		  uuid NOT NULL REFERENCES users(id),
+	friend_id	  uuid NOT NULL REFERENCES users(id),
+	requested_by  uuid NOT NULL REFERENCES users(id),
+	accepted	  boolean NOT NULL DEFAULT false,
+
+	CONSTRAINT	  chk_usrValue CHECK (user_id < friend_id),
+	CONSTRAINT	  chk_allone CHECK (user_id <> friend_id),
+	CONSTRAINT	  chk_reqByOne CHECK (requested_by = user_id OR requested_by = friend_id),
+	CONSTRAINT	  uq_oneTime UNIQUE (user_id, friend_id)
 );
 
 
